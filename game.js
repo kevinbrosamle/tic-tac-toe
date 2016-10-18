8f544466232619ff5de9d3ab4cb8cecd88d1d2f8
kevinbrosamle <kevin.brosamle@gmail.com>
2016-10-18 12:22:08 -0700

    Horizontal and vertical checking

--- game.js
+++ game.js
@@ -3,18 +3,44 @@ var boardDisplay =
   '[ ][ ][ ]\n' +
   '[ ][ ][ ]\n';
 
-var boardInfo = [
-
+var board = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', '']
 ]
 
 var createBoard = function() {
 
 }
 
-var checkWon = function(board) {
+var checkWon = function(board, player) {
+  // check horizontal
   for (var i = 0; i < board.length; i++) {
-
+    var gameWon = true;
+    for (var j = 0; j < board.length; j++) {
+      if (board[i][j] !== player) {
+        gameWon = false;
+      }
+    }
+    if (gameWon) {
+      return true;
+    }
   }
+
+  // check vertical
+  for (var i = 0; i < board[0].length; i++) {
+    var gameWon = true;
+    for (var j = 1; j < board.length; j++) {
+      if (board[i][j] !== player) {
+        gameWon = false;
+      }
+    }
+    if (gameWon) {
+      return true;
+    }
+  }
+
+  // check horizontal
 }
 
 var game = function() {
@@ -22,7 +48,6 @@ var game = function() {
   var player1 = 'X';
   var player2 = 'O';
   var currentPlayer = 'X';
-  console.log(board);
 
   while (gameWon === false) {
     console.log(board);
@@ -30,12 +55,14 @@ var game = function() {
     console.log('Input coordinates of next move')
     // move piece to input coordinates
     // check if current player wins
+    checkWon(board, currentPlayer);
     // switch player
     if (currentPlayer === 'O') {
       currentPlayer = 'X';
     } else {
       currentPlayer = 'O';
     }
+    gameWon = true;
   }
 
 }
