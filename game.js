ed84293c68e1f218447d95725d26b53c6d1fe121
kevinbrosamle <kevin.brosamle@gmail.com>
2016-10-18 11:53:40 -0700

    Build out game board

---
+++ game.js
@@ -0,0 +1,33 @@
+var boardDisplay =
+  '[ ][ ][ ]\n' +
+  '[ ][ ][ ]\n' +
+  '[ ][ ][ ]\n';
+
+var boardInfo = [
+
+]
+
+var createBoard = function() {
+
+}
+
+var checkWon = function(board) {
+  for (var i = 0; i < board.length; i++) {
+
+  }
+}
+
+var game = function() {
+  var playerWonGame = false
+  var player1 = 'X';
+  var player2 = 'O';
+  var turn = 'player1';
+  console.log(board);
+
+  // while (playerWonGame === false) {
+  //   console.log(board);
+  //   if ()
+  // }
+}
+
+game();
