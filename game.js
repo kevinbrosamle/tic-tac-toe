c763a609c8d4e889021924bcacb22196c5b8269b
kevinbrosamle <kevin.brosamle@gmail.com>
2016-10-18 12:16:49 -0700

    Logic for game flow

--- game.js
+++ game.js
@@ -18,16 +18,26 @@ var checkWon = function(board) {
 }
 
 var game = function() {
-  var playerWonGame = false
+  var gameWon = false
   var player1 = 'X';
   var player2 = 'O';
-  var turn = 'player1';
+  var currentPlayer = 'X';
   console.log(board);
 
-  // while (playerWonGame === false) {
-  //   console.log(board);
-  //   if ()
-  // }
+  while (gameWon === false) {
+    console.log(board);
+
+    console.log('Input coordinates of next move')
+    // move piece to input coordinates
+    // check if current player wins
+    // switch player
+    if (currentPlayer === 'O') {
+      currentPlayer = 'X';
+    } else {
+      currentPlayer = 'O';
+    }
+  }
+
 }
 
 game();
